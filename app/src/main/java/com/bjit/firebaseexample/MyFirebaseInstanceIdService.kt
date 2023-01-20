e661f53b72e7993ace93cfebe082d69299eacf38
Sarwar Miral <sarwar.miral@bjitgroup.com>
2023-01-20 11:19:29 +0600

    New service file is added

--- app/src/main/java/com/bjit/firebaseexample/MyFirebaseInstanceIdService.kt
+++ app/src/main/java/com/bjit/firebaseexample/MyFirebaseInstanceIdService.kt
@@ -2,11 +2,19 @@ package com.bjit.firebaseexample
 
 import android.util.Log
 import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+
 
 class MyFirebaseInstanceIdService : FirebaseMessagingService() {
 
     override fun onNewToken(token: String) {
         Log.d("FCM", "Refreshed token: $token")
+    }
 
+    override fun onMessageReceived(remoteMessage: RemoteMessage) {
+        Log.d("FCM", "From: " + remoteMessage.from)
+        Log.d("FCM", "Notification Message Body: " + remoteMessage.notification!!
+                .body
+        )
     }
 }
